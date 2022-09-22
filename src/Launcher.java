9204f4fcd0ae3d683d8255f811247d4ea834c718
WhiteColors_ <axel.casablanca@gmail.com>
2022-09-22 17:02:38 +0200

    Adding freq command

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Launcher {
     public static void main(String[] args) {
@@ -23,7 +27,38 @@ public class Launcher {
                         scanner.nextLine();
                         break;
                     case "freq":
+                        System.out.println("Entrez le chemin d'accès au fichier texte :");
+                        try{
+                            String content = Files.readString(Path.of(scanner.nextLine()));
+                            Stream<String> keys = Arrays.stream(content.replaceAll("[,.;:?']"," ").replaceAll("\n", "").toLowerCase().split(" "));
+                            HashMap<String, Integer> occurence = new HashMap<>();
+                            for(String value : keys.toList()){
+                                if(value.isBlank()){
+                                    continue;
+                                }
+                                if(!occurence.containsKey(value)){
+                                    occurence.put(value, 1);
+                                }else{
+                                    occurence.put(value, occurence.get(value)+1);
+                                }
+                            }
+                            for(int i=0;i<3;i++) {
+                                int max = -1;
+                                String name = null;
+                                for (Map.Entry<String, Integer> res : occurence.entrySet()) {
+                                    if(res.getValue()>max){
+                                        max = res.getValue();
+                                        name = res.getKey();
+                                    }
+                                }
+                                System.out.print(name+" ");
+                                occurence.values().remove(max);
+                            }
 
+
+                        } catch (IOException exception) {
+                            System.out.println("Unreadable file: "+exception);
+                        }
                         break;
                     default:
                         System.out.println("Unknown command");
